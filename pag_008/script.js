ab60dec9093b41acdf47cc4aebb7fbc91c44b4c0
Murillo Pinheiro de Oliveria <murillop.o06@gmail.com>
2022-05-04 17:06:46 -0300

    pag008

---
+++ pag_008/script.js
@@ -0,0 +1,35 @@
+const ratingContainer = document.querySelector(".rating-container") 
+const stars = Array.from(document.querySelectorAll(".star"))
+const submitRatingBtn = document.querySelector("button[type='submit']")
+
+stars.forEach(star => {
+    star.addEventListener("click", removeClass)
+    star.addEventListener("click",  toogleStarSelect)})
+
+    function removeClass() {
+        stars.forEach(star => {
+            star.classList.remove("star-selected")
+        })
+    }
+    
+function toogleStarSelect() {
+    if (!this.classList.contains("star-selected")) {
+        this.classList.add("star-selected")
+    } else {
+        this.classList.remove("star-selected")
+    }
+}
+
+function ratingFinished() {
+    ratingContainer.style.textAlign = "center"
+    ratingContainer.style.justifyContent = "center"
+    ratingContainer.innerHTML = `
+        <img src="images/illustration-thank-you.svg" class="thank-you-img" alt="thank-you">
+        <span class="highlight-text"><p>You selected Add rating here</p></span>
+        <h1>Thank you!</h1>
+        <p>We appreciate you taking the time to give a rating. If you ever need more support, 
+        don’t hesitate to get in touch!</p>
+    `
+}
+
+submitRatingBtn.addEventListener("click", ratingFinished)
