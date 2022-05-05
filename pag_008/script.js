5ea4d418344c133dab9e92b0261836d2ad576254
Murillo Pinheiro de Oliveria <83728277+MuriWolf@users.noreply.github.com>
2022-05-04 22:38:00 -0300

    pag 008 finished

--- pag_008/script.js
+++ pag_008/script.js
@@ -4,7 +4,17 @@ const submitRatingBtn = document.querySelector("button[type='submit']")
 
 stars.forEach(star => {
     star.addEventListener("click", removeClass)
-    star.addEventListener("click",  toogleStarSelect)})
+    star.addEventListener("click",  function() {
+        if (!this.classList.contains("star-selected")) {
+            this.classList.add("star-selected")
+            submitRatingBtn.addEventListener("click", ratingFinished)
+        } else {
+            this.classList.remove("star-selected")
+        }
+        valueSelected =  this.innerHTML
+    }) 
+
+})
 
     function removeClass() {
         stars.forEach(star => {
@@ -12,24 +22,15 @@ stars.forEach(star => {
         })
     }
     
-function toogleStarSelect() {
-    if (!this.classList.contains("star-selected")) {
-        this.classList.add("star-selected")
-    } else {
-        this.classList.remove("star-selected")
-    }
-}
-
 function ratingFinished() {
     ratingContainer.style.textAlign = "center"
     ratingContainer.style.justifyContent = "center"
-    ratingContainer.innerHTML = `
+    ratingContainer.innerHTML = 
+    `
         <img src="images/illustration-thank-you.svg" class="thank-you-img" alt="thank-you">
-        <span class="highlight-text"><p>You selected Add rating here</p></span>
+        <span class="highlight-text"><p>You selected ${valueSelected} out 5</p></span>
         <h1>Thank you!</h1>
         <p>We appreciate you taking the time to give a rating. If you ever need more support, 
         don’t hesitate to get in touch!</p>
     `
 }
-
-submitRatingBtn.addEventListener("click", ratingFinished)
