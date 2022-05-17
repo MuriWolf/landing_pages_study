ce1bae0f04287aa1454a00dd5a19084c73ff5f87
Murillo Pinheiro de Oliveria <murillop.o06@gmail.com>
2022-05-17 17:04:18 -0300

    advice generetor app - desktop version almostcomplete

--- pag_012/script.js
+++ pag_012/script.js
@@ -1,18 +1,15 @@
 const advice = document.querySelector("#advice")
+const adviceID = advice.querySelector(".advice-id")
+const adviceText = advice.querySelector(".advice-text")
 
 const refreshBtn = document.querySelector("#refresh-btn")
 
-fetch("https://api.adviceslip.com/advice")
+refreshBtn.addEventListener("click", function() {
+    fetch("https://api.adviceslip.com/advice")
     .then((resp) => resp.json())
     .then(data => {
-        const h2 = document.createElement("h2")
-        h2.innerText = `Advice # ${data.slip.id}`
-        advice.appendChild(h2)
-        const p = document.createElement("p")
-        p.innerText = `${data.slip.advice}`
-        advice.appendChild(p)
-    }) 
+        adviceID.innerText = `Advice # ${data.slip.id}`
+        adviceText.innerText = `"${data.slip.advice}"`
 
-refreshBtn.addEventListener("click", function() {
-    window.location.reload()
+    }) 
 })
