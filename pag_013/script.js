e34a4d2e4b47c170eeb00263836ac5b547347830
Murillo Pinheiro de Oliveria <83728277+MuriWolf@users.noreply.github.com>
2022-05-19 22:37:36 -0300

    pag 13

--- pag_013/script.js
+++ pag_013/script.js
@@ -4,10 +4,13 @@ dropdowns.forEach(dropdown => {
     dropdown.addEventListener("click", ()=> {
         console.log(dropdown)
         const dropdownContent = dropdown.querySelector(".dropdown-content")
+        const arrow = dropdown.querySelector(".arrow-icon")
+        arrow.style.transform = "rotate(180deg)"
         if (dropdownContent.classList.contains("hidden")) {
             dropdownContent.classList.remove("hidden")
         } else {
             dropdownContent.classList.add("hidden")
+            arrow.style.transform = "rotate(0deg)"
         }
 
     })  
